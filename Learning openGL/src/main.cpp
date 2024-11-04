9566727778a52896281dd9982d12c13a94bcfe62
Sett-0 <ampilov2000@mail.ru>
2024-11-04 04:34:41 +0300

    Добавьте файлы проекта.

---
+++ Learning openGL/src/main.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <random>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+// Generating random double in range
+std::random_device rd; // obtain a random number from hardware
+std::mt19937 gen(rd()); // seed the generator
+std::uniform_real_distribution<> distr(0, 1); // define the range
+
+int main() {
+	glfwInit();
+
+	// Using openGL version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Using core profile for openGL
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// For MacOS
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD!" << std::endl;
+		return -2;
+	}
+
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	// CREATING SHADERS
+	// Vertex Shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Checking if shader compilation went fine
+	int success;
+	char infoLog[512];
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Fragment Shader
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Checking if shader compilation went fine
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Linking created shaders into one program (aka pipline ?)
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Checking if linking shaders into program went fine
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Deleting the shader objects since we don't need them anymore
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// DRAWING AN OBJECT
+	// Triangle vertices in Screen Normalized Coordinates (each axis x, y and z in the range of (-1, 1))
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f,
+		 0.0f, -0.5f, 0.0f
+	};
+
+	// Allocating memory on GPU
+	unsigned int vertexBufferObject, vertexArrayObject;
+	glGenVertexArrays(1, &vertexArrayObject);
+	glGenBuffers(1, &vertexBufferObject);
+	
+	// Bind the Vertex Array Object first and set vertex buffer(s), and then convert vertex atribute(s)
+	glBindVertexArray(vertexArrayObject);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Telling openGL how to interpret the vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO 
+	// as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, 
+	// but this rarely happens. Modifying other VAOs requires a call to glBindVertexArray anyways 
+	// so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// RENDER LOOP
+	// Initial background color
+	glClearColor(0.07f, 0.07f, 0.07f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	while (!glfwWindowShouldClose(window)) {
+		// INPUT
+		processInput(window);
+
+		// RENDER
+		// Draw our first triangle
+		// Activating the program object to render it
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vertexArrayObject); // seeing as we only have a single VAO there's no need to bind it
+											  // every time, but we'll do it to keep things a bit moreorganized
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindVertexArray(0); // no need to unbind it every time
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Optional: de-allocate all resources once they're outlived their purpouse
+	glDeleteVertexArrays(1, &vertexArrayObject);
+	glDeleteBuffers(1, &vertexBufferObject);
+	glDeleteProgram(shaderProgram);
+
+	// GLFW: terminate, clearing all previously allocated GLFW resources
+	glfwTerminate();
+	return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		glClearColor((float)distr(gen), (float)distr(gen), (float)distr(gen), 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+	
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE) {
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+}
