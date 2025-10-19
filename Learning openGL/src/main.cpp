90a64b49339f40818e3eef61fbd4b982c43f7b97
Sett-0 <ampilov2000@mail.ru>
2025-10-19 10:33:29 +0300

    Making progress

--- Learning openGL/src/main.cpp
+++ Learning openGL/src/main.cpp
@@ -104,28 +104,46 @@ int main() {
 
 	// DRAWING AN OBJECT
 	// Triangle vertices in Screen Normalized Coordinates (each axis, x, y and z, lies in the range (-1, 1))
+	// Rectangle
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f,
-		 0.0f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f, // top right
+		-0.5f,  0.5f, 0.0f, // top left
+		-0.5f, -0.5f, 0.0f, // bottom left
+		 0.5f, -0.5f, 0.0f, // bottom right
+	};
+
+	unsigned int indices[] = { // note that we start from 0!
+		0, 1, 2,
+		2, 3, 0,
 	};
 
 	// Allocating memory on GPU
+	// Bind the Vertex Array Object first and set vertex buffer(s), and then convert vertex atribute(s)
 	unsigned int vertexBufferObject, vertexArrayObject;
 	glGenVertexArrays(1, &vertexArrayObject);
 	glGenBuffers(1, &vertexBufferObject);
-	
-	// Bind the Vertex Array Object first and set vertex buffer(s), and then convert vertex atribute(s)
+
+	// Creating Element Buffer Object
+	unsigned int ElementBufferObject;
+	glGenBuffers(1, &ElementBufferObject);
+
+	// Telling openGL how to interpret the vertex data
+	// Bind Vertex Array Object
 	glBindVertexArray(vertexArrayObject);
-
-
+	// Copy our vertices array in vertex buffer for openGL to use 
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Telling openGL how to interpret the vertex data
+	// Bind the Element Buffer Object
+	// Copy our indices array in element buffer for openGL to use 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ElementBufferObject);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Set the vertex attributes pointers
+	// args: pointer index of current VAO, n in vecn, data type of each component in the array, normalize (bool), stride, first data element offset pointer
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
+ 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO 
 	// as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -134,11 +152,21 @@ int main() {
 	// but this rarely happens. Modifying other VAOs requires a call to glBindVertexArray anyways 
 	// so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
+	
+	// ---> TLDR <---
+	// In VBO (vertex buffer object) we can store vertices data (position, color, texture coordinates),
+	// in EBO (element buffer object) we store indexes which represet line drawing order,
+	// in VAO (vertex array object) we specify how to read our data from VBO (again, which numbers are coordinates, which are colors etc).
+	// VBO, EBO and VAO are stored on the GPU!
+	// We also use binding functions to specify which BAO, EBO or VAO we are using right now.
+	// For different VBO data we need to use separate VAOs (for ex., we bind one VAO for vertex data and a second one for the colors).
 
 	// RENDER LOOP
 	// Initial background color
 	glClearColor(0.07f, 0.07f, 0.07f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Wireframe mode
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Default mode
 
 	while (!glfwWindowShouldClose(window)) {
 		// INPUT
@@ -150,8 +178,12 @@ int main() {
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vertexArrayObject); // seeing as we only have a single VAO there's no need to bind it
 											  // every time, but we'll do it to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// args: drawing mode, starting index, how many
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
 		//glBindVertexArray(0); // no need to unbind it every time
+		
+		//args: drawing mode, how many, indices type, offset in EBO
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
